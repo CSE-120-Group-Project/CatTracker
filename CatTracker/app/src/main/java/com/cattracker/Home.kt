bcf30d11a0b5644c3887270fbd8fdaabbbc24528
dblackwood80 <dblackwood80@gmail.com>
2018-10-23 09:01:44 -0700

    Merge Home.kt

--- CatTracker/app/src/main/java/com/cattracker/Home.kt
+++ CatTracker/app/src/main/java/com/cattracker/Home.kt
@@ -1,7 +1,9 @@
 package com.cattracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
@@ -9,5 +11,37 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Switch to closest stop page
+        val btnClosestStop = findViewById<Button>(R.id.btn_closest_stop);
+        btnClosestStop.setOnClickListener()
+        {
+            val intent = Intent(this, ClosestStop::class.java)
+            startActivity(intent)
+        }
+
+        //Switch to favorites page
+        val btnFavorites = findViewById<Button>(R.id.btn_favorites);
+        btnFavorites.setOnClickListener()
+        {
+            val intent = Intent(this, Favorites::class.java)
+            startActivity(intent)
+        }
+
+        //Switch to itinerary page
+        val btnItinerary = findViewById<Button>(R.id.btn_itinerary_search);
+        btnItinerary.setOnClickListener()
+        {
+            val intent = Intent(this, Itinerary::class.java)
+            startActivity(intent)
+        }
+
+        //Switch to timetables page
+        val btnTimetables = findViewById<Button>(R.id.btn_timetables);
+        btnTimetables.setOnClickListener()
+        {
+            val intent = Intent(this, Timetables::class.java)
+            startActivity(intent)
+        }
     }
 }

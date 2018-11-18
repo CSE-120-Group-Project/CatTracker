d16dc3a843affc13d180ca4c2388c1e3820768d5
rhules <38119764+rhules@users.noreply.github.com>
2018-11-17 18:05:28 -0800

    Update Map functions

--- CatTracker/app/src/main/java/com/cattracker/LineMarker.kt
+++ CatTracker/app/src/main/java/com/cattracker/LineMarker.kt
@@ -47,98 +47,118 @@ Class LineMarker :
 
 
     private lateinit var map: GoogleMap
-    private var lastSelectedMarker: Marker? = null
+
+    private var activeLine = Fastcat
 
     private var Heritage = mapOf {
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
-        "Yosemite_Ave_and_Cordova_Ave(Merced_County_Physicians" to LatLng(37.332089, -120.4625951),
-        "R_Street_Village_Apartments" to LatLng(37.3353841,-120.4867372)
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
+        "Yosemite Ave and Cordova Ave(Merced County Physicians" to LatLng(37.332089, -120.4625951),
+        "R Street Village Apartments" to LatLng(37.3353841,-120.4867372)
     }
 
     private var CampusTrax = mapOf{
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
         "Castle Air Park" to LatLng(37.3742624, -120.5766809)
     }
 
     private var C1 = mapOf{
-        "Granville_Apartments" to LatLng(37.315222, -120.502972),
-        "Walmart_on_Loughborough_Dr.(Pullout_past_Mistwood_Dr.)" to LatLng(37.316995, -120.499007),
-        "Meadows_Ave._&_Olivewood_Dr.(Food_Maxx)" to LatLng(37.318107, -120.490619),
-        //"Swiss_Colony_Apts." to LatLng(),
-        "G_St._&_W._Alexander_Ave.(Bus_Stop_on_G_by_Gas_Station)" to LatLng(37.315778, -120.469213),
-        "Rite_Aid/Walgreens" to LatLng(37.319633, -120.469128),
-        "El_Portal_&_G_Street(Bus_Stop_on_G)" to LatLng(37.327045, -120.469015),
-        "Mercy_Hospital/Tri-College" to LatLng(37.339455, -120.468750),
-        "Bellevue_Ranch_on_Arrow_Wood_Dr." to LatLng(37.3526891, -120.4781953),
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
-        "Mercy_Hospital/Tri-College" to LatLng(37.339455, -120.468750),
-        "El_Portal_&_G_Street(Bus_Stop_on_G)" to LatLng(37.327045, -120.469015),
-        "G_St._&_W._Alexander_Ave.(Bus_Stop_on_G_by_Gas_Station)" to LatLng(37.315778, -120.469213),
+        "Granville Apartments" to LatLng(37.315222, -120.502972),
+        "Walmart on Loughborough Dr.(Pullout past Mistwood Dr.)" to LatLng(37.316995, -120.499007),
+        "Meadows Ave. & Olivewood Dr.(Food Maxx)" to LatLng(37.318107, -120.490619),
+        //"Swiss Colony Apts." to LatLng(),
+        "G St. & W. Alexander Ave.(Bus Stop on G by Gas Station)" to LatLng(37.315778, -120.469213),
+        "Rite Aid/Walgreens" to LatLng(37.319633, -120.469128),
+        "El Portal & G Street(Bus Stop on G)" to LatLng(37.327045, -120.469015),
+        "Mercy Hospital/Tri-College" to LatLng(37.339455, -120.468750),
+        "Bellevue Ranch on Arrow Wood Dr." to LatLng(37.3526891, -120.4781953),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
+        "Mercy Hospital/Tri-College" to LatLng(37.339455, -120.468750),
+        "El Portal & G Street(Bus Stop on G)" to LatLng(37.327045, -120.469015),
+        "G St. & W. Alexander Ave.(Bus Stop on G by Gas Station)" to LatLng(37.315778, -120.469213),
         //"Swiss Colony"
-        "Meadows_Ave._&_Olivewood_Dr.(Food_Maxx)" to LatLng(37.318107, -120.490619),
+        "Meadows Ave. & Olivewood Dr.(Food Maxx)" to LatLng(37.318107, -120.490619),
         //"Walmart"
     }
 
     private var C2 = mapOf{
-        "R_Street_Village_Apartments" to LatLng(37.3353841,-120.4867372)
-        "Compass_Pointe_Apts" to LatLng (37.335707, -120.490445),
-        "Buena_Vista_Dr." to LatLng(37.326252, -120.502633),
-        "Merced_Mall_Target" to LatLng(37.323303, -120.485577),
-        "Villages_Apts._M_Street" to LatLng(37.324417, -120.478281),
-        "Merced_College_The_Bus_Terminal" to LatLng(37.334558, -120.477976),
-        "Ironstone_Dr." to LatLng(37.342589, -120.4806),
-        "Bellevue_Ranch_on_Arrow_Wood_Dr." to LatLng(37.3526891, -120.4781953),
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
+        "R Street Village Apartments" to LatLng(37.3353841,-120.4867372)
+        "Compass Pointe Apts" to LatLng (37.335707, -120.490445),
+        "Buena Vista Dr." to LatLng(37.326252, -120.502633),
+        "Merced Mall Target" to LatLng(37.323303, -120.485577),
+        "Villages Apts. M Street" to LatLng(37.324417, -120.478281),
+        "Merced College The Bus Terminal" to LatLng(37.334558, -120.477976),
+        "Ironstone Dr." to LatLng(37.342589, -120.4806),
+        "Bellevue Ranch on Arrow Wood Dr." to LatLng(37.3526891, -120.4781953),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
     }
 
     private var E1 = mapOf{
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
     }
 
     private var E2 = mapOf{
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
     }
 
     private var Fastcat = mapOf{
-        "Moraga_Subdivision" to LatLng(37.332027, -120.438567),
-        "Yosemite_Church(McKee_Rd.)" to LatLng(37.332027, -120,442899),
-        "University_Surgery_Center" to LatLng(37.332207, -120.451404),
-        "Starbucks/Promenade_Center" to LatLng(37.332027, -120.460492),
-        "Mercy_Hospital/Tri-College" to LatLng(37.339455, -120.468750),
-        "Cardella_Rd_&_M_Street" to LatLng(37.347495, -120.477322),
-        "Bellevue_Ranch_on_Arrow_Wood_Dr." to LatLng(37.3526891, -120.4781953),
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
-        "Bellevue_Ranch_on_Arrow_Wood_Dr." to LatLng(37.3526891, -120.4781953),
-        "Mercy_Hospital/Tri-College" to LatLng(37.339455, -120.468750),
-        "Yosemite_Ave_and_Cordova_Ave(Merced_County_Physicians" to LatLng(37.332089, -120.4625951),
-        "University_Surgery_Center" to LatLng(37.332207, -120.451404),
-        "Yosemite_Church(McKee_Rd.)" to LatLng(37.332027, -120,442899),
-        "Moraga_Subdivision" to LatLng(37.332027, -120.438567),
+        "Moraga Subdivision" to LatLng(37.332027, -120.438567),
+        "Yosemite Church(McKee Rd.)" to LatLng(37.332027, -120,442899),
+        "University Surgery Center" to LatLng(37.332207, -120.451404),
+        "Starbucks/Promenade Center" to LatLng(37.332027, -120.460492),
+        "Mercy Hospital/Tri-College" to LatLng(37.339455, -120.468750),
+        "Cardella Rd & M Street" to LatLng(37.347495, -120.477322),
+        "Bellevue Ranch on Arrow Wood Dr." to LatLng(37.3526891, -120.4781953),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
+        "Bellevue Ranch on Arrow Wood Dr." to LatLng(37.3526891, -120.4781953),
+        "Mercy Hospital/Tri-College" to LatLng(37.339455, -120.468750),
+        "Yosemite Ave and Cordova Ave(Merced County Physicians" to LatLng(37.332089, -120.4625951),
+        "University Surgery Center" to LatLng(37.332207, -120.451404),
+        "Yosemite Church(McKee Rd.)" to LatLng(37.332027, -120,442899),
+        "Moraga Subdivision" to LatLng(37.332027, -120.438567),
     }
 
     private var G = mapOf{
-        "R_Street_Village_Apartments" to LatLng(37.3353841,-120.4867372)
-        "Compass_Pointe_Apts" to LatLng (37.335707, -120.490445),
-        "Merced_College_The_Bus_Terminal" to LatLng(37.334558, -120.477976),
-        "Ironstone_Dr." to LatLng(37.342589, -120.4806),
-        "Bellevue_Ranch_on_Arrow_Wood_Dr." to LatLng(37.3526891, -120.4781953),
-        "Mammoth_Lakes_Rd." to LatLng(37.363256, -120.429404),
-        "Student_Activities_&_Athletics_Center" to LatLng(37.3654274, -120.4262007),
-        "Emigrant_Pass_at_Scholars_Lane" to LatLng(37.3637409, -120.4305694),
+        "R Street Village Apartments" to LatLng(37.3353841,-120.4867372)
+        "Compass Pointe Apts" to LatLng (37.335707, -120.490445),
+        "Merced College The Bus Terminal" to LatLng(37.334558, -120.477976),
+        "Ironstone Dr." to LatLng(37.342589, -120.4806),
+        "Bellevue Ranch on Arrow Wood Dr." to LatLng(37.3526891, -120.4781953),
+        "Mammoth Lakes Rd." to LatLng(37.363256, -120.429404),
+        "Student Activities & Athletics Center" to LatLng(37.3654274, -120.4262007),
+        "Emigrant Pass at Scholars Lane" to LatLng(37.3637409, -120.4305694),
         "Amtrak" to LatLng(37.307412, -120.477543),
-        "K_St._Between_18th_St._&_19th_St." to LatLng(37.302271, -120.481111),
+        "K St. Between 18th St. & 19th St." to LatLng(37.302271, -120.481111),
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?){
+        map = googlemap ?: return
+
+        addMarkers()
+    }
+
+    private fun addMarkers(){
+        val placeDetailsMap = mutableMapOf(
+                for (key in activeLine){
+
+                    $key to PlaceDetails(
+                            position = activeLine.getValue($key),
+                            title = $key,
+                            draggable = false
+                            )
+                }
+        )
     }
